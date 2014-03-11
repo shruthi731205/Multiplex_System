ea7f34e6f69a81fb66af0548d7d3bd9f2249e67d
shruthi <shruthi@shruthi-virtual-machine.(none)>
2014-03-11 20:04:36 +0530

    config folder added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MultiplexSystem::Application.config.secret_key_base = '723f4b3491b6d6080381d9f5910117df6a91eaeef0980a25e1876e6664ef39e289282143d409c3ca06ef4ca82e90eb6f3ea1eb9fba152e1391a28c07ab39f64d'
